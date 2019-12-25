e766cdaf67a24bd7d89952b4fe7d26a482472ea1
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-25 22:28:12 +0545

    Test CryptoJS encrypt and decrypt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crypto-js-angular-demo';
+  object = {
+    name: 'Elvin Shrestha',
+    age: 20,
+  };
+  encrypted;
+  decrypted;
+
+  ngOnInit(): void {
+    this.encrypted = CryptoJS.AES.encrypt(JSON.stringify(this.object), this.title, null).toString();
+    this.decrypted = JSON.parse(CryptoJS.AES.decrypt(this.encrypted, this.title, null).toString(CryptoJS.enc.Utf8));
+  }
 }
